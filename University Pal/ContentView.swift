4c45de868e8451e1ca3f8ec773c59d1e9d0fe676
asluis <asluisinfo@gmail.com>
2021-10-03 22:34:47 -0700

    Initial Commit

---
+++ University Pal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  University Pal
+//
+//  Created by Luis Alvarez on 10/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ University Pal/University_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  University_PalApp.swift
+//  University Pal
+//
+//  Created by Luis Alvarez on 10/3/21.
+//
+
+import SwiftUI
+
+@main
+struct University_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
