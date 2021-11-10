5ec410f0a3f1850791929c708db5247ae25d56f5
atsuyay <60360870+atsuyay@users.noreply.github.com>
2021-11-09 18:46:45 -0800

    restore ContextView

---
+++ src/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  University Pal
+//
+//  Created by Luis Alvarez on 10/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hell world")
+            .padding()
+    }
+}
+
+
+
