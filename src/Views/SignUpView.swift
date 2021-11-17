c9f8567755d130abdb26c7971f1a63ea392ff64e
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2021-11-16 20:18:09 -0800

    Cleaning up code and establishing form

--- src/Views/SignUpView.swift
+++ src/Views/SignUpView.swift
@@ -8,31 +8,41 @@
 import SwiftUI
 
 struct SignUpView: View {
+    
+    @State private var email = ""
+    @State private var name = ""
+    
     var body: some View {
         GeometryReader{ geo in
-            VStack{
-                NavigationView{
-                    VStack{
-                        Text("Hello there")
-                            .toolbar{
-                                ToolbarItem(placement: .principal){
-                                    Image("Books")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: geo.size.width, height: geo.size.height)
-                                        .edgesIgnoringSafeArea(.top)
-                                    
-                                }
+            ZStack{
+                VStack{
+                    Section(header: Group{
+                        ZStack{
+                            Image("Books")
+                                .resizable()
+                                .frame(width: geo.size.width, height: geo.size.height * 0.2)
+                                .edgesIgnoringSafeArea(.top)
+                            Text("Sign up")
+                                .foregroundColor(.white)
+                                .font(.system(size: 65))
+                                .bold()
+                                .padding(.bottom, 60)
+                        }
+                    }){
+                        Form{
+                            Section(header: Text("Name").font(.headline)){
+                                TextField("Name", text: $name)
                             }
-                        Text("Bobby")
+                        }.padding(.top, -geo.size.height * 0.07)
                     }
+                    
                 }
-                .navigationBarTitleDisplayMode(.inline)
+                LineAccent(Orientation: .Vertical, percentLength: 0.9, lineColor: .yellow, circleColor: .black, lineWidth: 5)
+                    .padding(.leading, 5)
+                LineAccent(Orientation: .Horizontal, percentLength: 0.5, lineColor: .yellow, circleColor: .black, lineWidth: 5)
+                    .rotationEffect(Angle(degrees: 180))
             }
-            LineAccent(Orientation: .Vertical, percentLength: 0.9, lineColor: .yellow, circleColor: .black, lineWidth: 5)
-                .padding(.leading, 5)
-            LineAccent(Orientation: .Horizontal, percentLength: 0.5, lineColor: .yellow, circleColor: .black, lineWidth: 5)
-                .rotationEffect(Angle(degrees: 180))
+            
         }
     }
 }
