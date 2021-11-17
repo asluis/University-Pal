f99e230b4689dc547667f0cc6f8927b8343d8605
atsuyay <60360870+atsuyay@users.noreply.github.com>
2021-11-16 19:29:06 -0800

    ListingUI (first commit)

---
+++ src/Views/ListingImage.swift
@@ -0,0 +1,20 @@
+//
+//  ListingImage.swift
+//  University Pal
+//
+//  Created by 矢野敦也 on 2021/11/16.
+//
+
+import SwiftUI
+
+struct ListingImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListingImage_Previews: PreviewProvider {
+    static var previews: some View {
+        ListingImage()
+    }
+}
---
+++ src/Views/Listinginfo.swift
@@ -0,0 +1,20 @@
+//
+//  Listinginfo.swift
+//  University Pal
+//
+//  Created by 矢野敦也 on 2021/11/16.
+//
+
+import SwiftUI
+
+struct Listinginfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Listinginfo_Previews: PreviewProvider {
+    static var previews: some View {
+        Listinginfo()
+    }
+}
