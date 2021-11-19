95a38883b45152a2158eab89e6e9899b2841f7d9
atsuyay <60360870+atsuyay@users.noreply.github.com>
2021-11-18 19:20:07 -0800

    Change Build Settings to run on M1 Macbook

--- src/Views/Listinginfo.swift
+++ src/Views/Listinginfo.swift
@@ -1,20 +1,95 @@
 //
-//  Listinginfo.swift
+//  ListingImage.swift
 //  University Pal
-//
-//  Created by 矢野敦也 on 2021/11/16.
 //
 
 import SwiftUI
 
-struct Listinginfo: View {
+struct ListingInfo: View {
+    @State private var title = ""
+    @State private var Author = ""
+    @State private var ISBN = 0
+    @State private var Subject = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader{ geo in
+            ZStack{
+                VStack{
+                    Section(header: Group{
+                        ZStack{
+                            Image("Books")
+                                .resizable()
+                                .frame(width: geo.size.width, height: geo.size.height * 0.2)
+                                .edgesIgnoringSafeArea(.top)
+                            Text("Listing")
+                                .foregroundColor(.white)
+                                .font(.system(size: 65))
+                                .bold()
+                                .padding(.bottom, 60)
+                        }
+                    }){
+                        Form{
+                            Section(header: Text("Title").font(.headline)){
+                                TextField("Title", text: $title)
+                            }
+                            Section(header: Text("Author").font(.headline)){
+                                TextField("Author", text: $Author)
+                            }
+                            Section(header: Text("ISBN").font(.headline)){
+                                TextField("ISBN", value: $ISBN, formatter: NumberFormatter())
+                            }
+                            Section(header: Text("Subject").font(.headline)){
+                                TextField("Subject", text: $Subject)
+                            }
+                        }.padding(.vertical, -geo.size.height * 0.07)
+                    }
+                }
+                HStack{
+                    //Cancel button
+                    Button(action: {
+                        // TODO: Add action here
+                    }) {
+                        HStack{
+                            Image(systemName: "multiply")
+                            Text("Cancel")
+                                .frame(width: 70, height: 30)
+                        }
+                    }.buttonStyle(GradientButtonStyle())
+                    Spacer()
+                    
+                    //List button
+                    Button(action: {
+                        // TODO: Add action here
+                    }) {
+                        HStack{
+                            Image(systemName: "plus.square.fill.on.square.fill")
+                            Text("List")
+                                .frame(width: 70, height: 30)
+                        }
+                    }.buttonStyle(GradientButtonStyle())
+                    
+                }.padding(.top, -geo.size.height * -0.7)
+                    .padding(.horizontal, 40)
+
+            }
+        }
     }
 }
 
-struct Listinginfo_Previews: PreviewProvider {
+struct GradientButtonStyle: ButtonStyle{
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .foregroundColor(Color.white)
+            .padding()
+            .background(configuration.isPressed ? Color.green : Color("MangoYellow"))
+            .cornerRadius(20)
+            .scaleEffect(configuration.isPressed ? 1.3 : 1.0)
+    }
+}
+
+
+struct ListingInfo_Previews: PreviewProvider {
     static var previews: some View {
-        Listinginfo()
+        ListingInfo()
     }
 }
