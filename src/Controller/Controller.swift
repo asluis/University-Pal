89c053a18cc37981f4d68f9d9ebd622743c430d7
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2021-11-21 22:00:50 -0800

    Defining Controller class and ViewBank enum

---
+++ src/Controller/Controller.swift
@@ -0,0 +1,18 @@
+//
+//  Controller.swift
+//  University Pal
+//
+//  Created by Luis Alvarez Sanchez on 11/21/21.
+//
+
+import Foundation
+
+class Controller: ObservableObject{
+    
+    @Published var currView:ViewBank = .WelcomeScreen
+    
+    // TODO: Define user and model within controller
+    
+    
+    
+}
