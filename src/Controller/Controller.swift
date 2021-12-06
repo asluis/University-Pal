741efda25ac74bcb91caf0c170942ad549e0a031
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2021-12-05 23:14:40 -0800

    Added temporary book to controller

--- src/Controller/Controller.swift
+++ src/Controller/Controller.swift
@@ -14,6 +14,9 @@ class Controller: ObservableObject{
     @Published var currUser:User = User()
     // TODO: Define model within controller
     
+    // Temporary book for storing a book's details between views, this book is not automatically uploaded to firebase
+    @Published var tempBook:Book = Book()
+    
     
     // Obtains and returns the view to be displayed
     func determineView() -> AnyView{
