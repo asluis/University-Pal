84e4b0a8cbdcf3444a597953f49a71f2293a9aeb
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2021-12-07 16:42:29 -0800

    Declaring Tab view files and fixing errors in other files

---
+++ src/Views/Tab/TabItem.swift
@@ -0,0 +1,22 @@
+//
+//  TabItem.swift
+//  University Pal
+//
+//  Created by Luis Alvarez Sanchez on 12/7/21.
+//
+
+import SwiftUI
+
+struct TabItem: View {
+    var body: some View {
+        VStack{
+            Image(systemName: "homekit")
+        }
+    }
+}
+
+struct TabItem_Previews: PreviewProvider {
+    static var previews: some View {
+        TabItem()
+    }
+}
---
+++ src/Views/Tab/TabView.swift
@@ -0,0 +1,20 @@
+//
+//  TabView.swift
+//  University Pal
+//
+//  Created by Luis Alvarez Sanchez on 12/7/21.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabView()
+    }
+}
