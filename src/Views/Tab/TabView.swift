cdaecd37b45136c1401cb687dc5b15fa5f136299
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2021-12-07 18:23:49 -0800

    Added tab view definitions and defined TabItem

--- src/Views/Tab/TabView.swift
+++ src/Views/Tab/TabView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct TabView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader{ geo in
+            VStack{
+                Spacer()
+                Text("Sample")
+                Spacer()
+                HStack{
+                    TabItem(width: geo.size.width/5, height: geo.size.height/28, systemIconName: "house", tabName: "Home")
+                    TabItem(width: geo.size.width/5, height: geo.size.height/28, systemIconName: "message", tabName: "Chat")
+                    TabItem(width: geo.size.width/5, height: geo.size.height/28, systemIconName: "person.fill", tabName: "Profile")
+                    TabItem(width: geo.size.width/5, height: geo.size.height/28, systemIconName: "waveform", tabName: "Logout")
+                }
+                    .frame(width: geo.size.width, height: geo.size.height/8)
+                    .background(Color.gray.shadow(radius: 2))
+            }.edgesIgnoringSafeArea(.bottom)
+        }
     }
 }
 
