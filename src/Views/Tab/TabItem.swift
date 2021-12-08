b2274b4014c6e68f51d22f82383c23ab42ec4486
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2021-12-07 18:23:49 -0800

    Added tab view definitions and defined TabItem

--- src/Views/Tab/TabItem.swift
+++ src/Views/Tab/TabItem.swift
@@ -8,15 +8,27 @@
 import SwiftUI
 
 struct TabItem: View {
+    let width, height: CGFloat
+    let systemIconName, tabName: String
+         
+         
     var body: some View {
-        VStack{
-            Image(systemName: "homekit")
+        VStack {
+            Image(systemName: systemIconName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: width, height: height)
+                .padding(.top, 10)
+            Text(tabName)
+                .font(.footnote)
+            Spacer()
         }
-    }
+        .padding(.horizontal, -4)
+     }
 }
 
 struct TabItem_Previews: PreviewProvider {
     static var previews: some View {
-        TabItem()
+        TabItem(width: 50, height: 50, systemIconName: "homekit", tabName: "Home")
     }
 }
